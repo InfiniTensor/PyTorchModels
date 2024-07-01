47efa136dddf6a5853358a9f120a11954dc4f4de
JYMiracle305 <604951424@qq.com>
2024-07-01 16:09:09 +0800

    reset SSD model

--- Detection/SSD/create_data_lists.py
+++
@@ -1,6 +0,0 @@
-from utils import create_data_lists
-
-if __name__ == '__main__':
-    create_data_lists(voc07_path='./VOCdevkit/VOC2007',
-                      voc12_path='./VOCdevkit/VOC2012',
-                      output_folder='./')
