eca1082d04caf682ed4b0cacf6311956a3f92169
zhangyue <zhangyue@qiyuanlab.com>
2024-07-11 18:07:23 +0800

    check bert albert

--- NLP/HuggingFace/qa.py
+++ NLP/HuggingFace/qa.py
@@ -424,7 +424,7 @@ def main():
         if args.test_file is not None:
             data_files["test"] = args.test_file
         extension = args.train_file.split(".")[-1]
-        raw_datasets = load_dataset('./squad_v2.py', data_files=data_files)
+        raw_datasets = load_dataset('./squad_v2.py', data_files=data_files, trust_remote_code=True)
     # See more about loading any type of standard or custom dataset (from files, python dict, pandas DataFrame, etc) at
     # https://huggingface.co/docs/datasets/loading_datasets.
 
