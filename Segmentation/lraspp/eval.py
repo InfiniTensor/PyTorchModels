9579fc3d4d180fdf7e7aaaac9db3aaf373fdf8a7
zhangyue <zhangyue@qiyuanlab.com>
2024-07-15 16:01:49 +0800

    update lraspp scripts

--- Segmentation/lraspp/eval.py
+++ Segmentation/lraspp/eval.py
@@ -5,7 +5,6 @@ import sys
 
 from pathlib import Path
 from torchvision import transforms, datasets
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 from torchvision.models.segmentation import lraspp_mobilenet_v3_large
 
 sys.path.append("../")
@@ -46,19 +45,18 @@ def voc_colormap_to_label(mask):
         label_mask[np.all(mask == color, axis=-1)] = idx
     return torch.tensor(label_mask, dtype=torch.long)
 
-num_classes = 21
-intersection = torch.zeros(num_classes, num_classes)
-union = torch.ones(num_classes, num_classes)
 
-
-def eval(num_classes):
+def eval(model,
+         num_classes,
+         val_loader,
+         device):
     evaluator = Evaluator(num_classes)
     evaluator.reset()
-    model = lraspp_mobilenet_v3_large(pretrained=True, num_classes=num_classes).to(device)
+    model = model.to(device)
     model.eval()
 
     with torch.no_grad():
-        for _, batch in enumerate(datasetloader):
+        for _, batch in enumerate(val_loader):
             input, target = batch
             input = input.to(device)
             output = model(input)
@@ -71,41 +69,49 @@ def eval(num_classes):
 
         print(evaluator.Mean_Intersection_over_Union())
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--device", type=str, default=("cuda" if torch.cuda.is_available() else "cpu"),
+                        choices=["cuda", "cpu"], 
+                        help="Device to use for training and inference.")
+    parser.add_argument("--input_size", default=256, type=int,
+                        help="Input size")
+    parser.add_argument("--dataset_path", type=str, default="../data")
+    parser.add_argument("--VOC_year", type=str, default="2007")
+    parser.add_argument("--num_classes", type=int, default=21)
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--device", type=str, default="cpu",
-                    choices=["cuda", "cpu"], 
-                    help="Device to use for training and inference.")
-parser.add_argument("--input_size", default=256, type=int,
-                    help="Input size")
-parser.add_argument("--classes", type=int, default=21, 
-                    help="Num of classes")
-parser.add_argument("--dataset_path", type=str, default="../data")
-parser.add_argument("--VOC_year", type=str, default="2007")
-args = parser.parse_args()
+    print(vars(args))
 
-data_folder = args.dataset_path
-year = args.VOC_year
-model_path = Path("model") / f"lraspp_b4_ep100.pt"
-transform = transforms.Compose([
-    transforms.Resize((args.input_size, args.input_size)),
-    transforms.ToTensor(), 
+    data_folder = args.dataset_path
+    year = args.VOC_year
+
+    # Prepare data
+    transform = transforms.Compose([
+        transforms.Resize((args.input_size, args.input_size)),
+        transforms.ToTensor(), 
+        ])
+
+    target_transform = transforms.Compose([
+        transforms.Resize((args.input_size, args.input_size)),
+        transforms.Lambda(lambda img: voc_colormap_to_label(img))
     ])
 
-target_transform = transforms.Compose([
-    transforms.Resize((args.input_size, args.input_size)),
-    transforms.Lambda(lambda img: voc_colormap_to_label(img))
-])
+    dataset = datasets.VOCSegmentation(
+        data_folder,
+        year=year,
+        download=False,
+        image_set="test",
+        transform=transform,
+        target_transform=target_transform
+    )
+    datasetloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
 
-dataset = datasets.VOCSegmentation(
-    data_folder,
-    year=year,
-    download=False,
-    image_set="test",
-    transform=transform,
-    target_transform=target_transform
-)
-datasetloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
+    # Prepare model
+    model = lraspp_mobilenet_v3_large(pretrained=True, num_classes=args.num_classes) 
+    
+    print(f'[INFO] Start inference on {args.device}.')
+    eval(model, args.num_classes, datasetloader, args.device)
 
 if __name__ == "__main__":
-    eval(args.classes)
+    main()
