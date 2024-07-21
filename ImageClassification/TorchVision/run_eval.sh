261d8299b04a0e1965ab0e5d0af2138ffbc82a66
zhangyue <zhangyue@qiyuanlab.com>
2024-07-21 18:07:10 +0800

    fix shell

--- ImageClassification/TorchVision/run_eval.sh
+++ ImageClassification/TorchVision/run_eval.sh
@@ -8,22 +8,19 @@ set -e
 export CUDA_VISIBLE_DEVICES=0
 
 ARCH=""
-LOG_FILE=""
 
 # Help message
 usage() {
     echo "Usage: $0 -a ARCH"
     echo "  -a ARCH, --arch ARCH       Model architecture (e.g., AlexNet, ResNet18, etc.)"
     echo "  -h, --help                 Display this help and exit"
-    echo "  -l LOG_FILE, --log LOG_FILE Log file"
     exit 1
 }
 
 # Parse command line arguments
-while getopts "a:l:h" opt; do
+while getopts "a:h" opt; do
     case $opt in
         a) ARCH=$OPTARG ;;
-        l) LOG_FILE=$OPTARG ;;
         h) usage ;;
         *) usage ;;
     esac
@@ -43,15 +40,8 @@ else
     ln -s /data1/shared/Dataset/imagenet2012 ../data
 fi
 
-# Define log file with ARCH included
-if [ -z "$LOG_FILE" ]; then
-    LOG_FILE="pytorch-${ARCH}-eval-gpu${CUDA_VISIBLE_DEVICES}.log"
-    echo "Logs saved in defaut log file ${LOG_FILE}"
-else 
-    echo "Logs saved in ${LOG_FILE}"
-fi
 
-echo "Evaluating Start: $(date +'%m/%d/%Y %T')" > ${LOG_FILE}
+echo "Evaluating Start: $(date +'%m/%d/%Y %T')"
 
 # 单机单卡推理
 echo "Evaluating $ARCH..."
@@ -61,6 +51,6 @@ python main.py \
     --batch-size 64 \
     --pretrained \
     --evaluate \
-    ../data/imagenet2012 2>&1 | tee -a $LOG_FILE
+    ../data/imagenet2012
 
-echo "Evaluating Finish: $(date +'%m/%d/%Y %T')" >> ${LOG_FILE}
+echo "Evaluating Finish: $(date +'%m/%d/%Y %T')" 
