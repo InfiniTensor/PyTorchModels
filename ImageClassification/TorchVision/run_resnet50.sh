c605f7488ee59c6c24e589387780df14ee31bfa0
zhangyue <zhangyue@qiyuanlab.com>
2024-07-21 17:51:54 +0800

    update shell

--- ImageClassification/TorchVision/run_resnet50.sh
+++ ImageClassification/TorchVision/run_resnet50.sh
@@ -1,26 +1,22 @@
-#!/bin/bash
-
-# 脚本用于运行 Torchvision 中所有分类模型的的训练
-# 数据集放在 /data1/shared/Dataset/imagenet2012
-
-set -e
-
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 ARCH=""
+LOG_FILE=""
 
 # Help message
 usage() {
     echo "Usage: $0 -a ARCH"
     echo "  -a ARCH, --arch ARCH       Model architecture (e.g., AlexNet, ResNet18, etc.)"
     echo "  -h, --help                 Display this help and exit"
+    echo "  -l LOG_FILE, --log LOG_FILE Log file"
     exit 1
 }
 
 # Parse command line arguments
-while getopts "a:h" opt; do
+while getopts "a:l:h" opt; do
     case $opt in
         a) ARCH=$OPTARG ;;
+        l) LOG_FILE=$OPTARG ;;
         h) usage ;;
         *) usage ;;
     esac
@@ -40,10 +36,16 @@ else
     ln -s /data1/shared/Dataset/imagenet2012 ../data
 fi
 
-echo "Training Start: $(date +'%m/%d/%Y %T')" 
+# Define log file with ARCH included
+if [ -z "$LOG_FILE" ]; then
+    LOG_FILE="pytorch-${ARCH}-train-gpu${CUDA_VISIBLE_DEVICES}.log"
+    echo "Logs saved in defaut log file ${LOG_FILE}"
+else 
+    echo "Logs saved in ${LOG_FILE}"
+fi
 
-# 4 card training
-echo "Training $ARCH..."
+echo "Training Start: $(date +'%m/%d/%Y %T')" > ${LOG_FILE}
+
 python main.py \
     -a $ARCH \
     --dist-backend 'nccl' \
@@ -52,6 +54,6 @@ python main.py \
     --world-size 1 \
     --rank 0 \
     --batch-size 64 \
-    ../data/imagenet2012
+    ../data/imagenet2012  2>&1 | tee -a $LOG_FILE
 
-echo "Training Finish: $(date +'%m/%d/%Y %T')"
+echo "Training Finish: $(date +'%m/%d/%Y %T')" >> ${LOG_FILE}
