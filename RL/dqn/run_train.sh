4207b61410cffdae44e623ee4ae9b53922121bf6
zhangyue <zhangyue@qiyuanlab.com>
2024-07-23 18:04:55 +0800

    add dqn

---
+++ RL/dqn/run_train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+export CUDA_VISIBLE_DEVICES=0
+
+saved_dir=$1
+num_episodes=$2
+lr=$3
+
+python main.py \
+       --save_path $saved_dir \
+       --num_episodes $num_episodes \
+       --lr=$lr
+
+# bash run_train.sh checkpoints 100 0.0001
