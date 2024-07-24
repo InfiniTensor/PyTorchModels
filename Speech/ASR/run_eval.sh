88a3978f07beb59435370adf6666e4d8331f7d3d
qinyiqun <qinyiqun@outlook.com>
2024-07-24 13:58:54 +0800

    modify cache path

--- Speech/ASR/run_eval.sh
+++ Speech/ASR/run_eval.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ -e "../data/data_thchs30" ]; then
-    echo "../data/data_thchs30 exists"
+if [ -e "./cache/data_thchs30" ]; then
+    echo "./cache/data_thchs30 exists"
 else
     # 创建软连接
-    # ln -s /data1/shared/Dataset/data_thchs30 ../data
-    mkdir ../data
-    ln -s /home/qinyiqun/data_thchs30 ../data/data_thchs30
+    mkdir ./cache
+    ln -s /data1/shared/Dataset/data_thchs30 ./cache/data_thchs30
+    # ln -s /home/qinyiqun/data_thchs30 ../data/data_thchs30
 fi
 
 python data_preprocess.py
