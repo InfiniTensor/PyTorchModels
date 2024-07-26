caf80709486237954f97d444aaef097859135f1f
Bolun <chamberlain0w0@gmail.com>
2024-07-26 13:57:05 +0800

    fix dataset path

--- Speech/deepspeech2/run_train.sh
+++ Speech/deepspeech2/run_train.sh
@@ -3,7 +3,7 @@
 if [ -e "../data/data_thchs30" ]; then
     echo "../data/data_thchs30 exists"
 else
-    ln -s /data1/shared/Dataset/data_thchs30 ../data_thchs30
+    ln -s /data1/shared/Dataset/data_thchs30 ../data/data_thchs30
 fi
 
 if [ -e "./cache" ]; then
